d8a7524180119b883674760517c400f2807d5d7c
zanmato <zanmato1984@gmail.com>
2022-07-28 15:02:46 +0800

    Refine

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	nameFlag := flag.String("name", "test", "Name of this bake.")
-	imagePathFlag := flag.String("image_path", "", "Path to the image to render.")
-	dbFlag := flag.String("db", "", "Database connection string.")
-	skipPrepareFlag := flag.Bool("skip_prepare", false, "Skip prepare steps.")
-	alignSecFlag := flag.Int("align_sec", 0, "Seconds to align to start baking.")
+	nameFlag := flag.String("name", "keyviz_baker", "Name of this bake.")
+	imagePathFlag := flag.String("image_path", "", "Path to the image to render. Must be PNG format.")
+	dbFlag := flag.String("db", "", "Database connection string. E.g. 'root:@tcp(127.0.0.1:4000)/test'.")
+	skipPrepareFlag := flag.Bool("skip_prepare", false, "Skip preparation.")
+	alignSecFlag := flag.Int("align_sec", 0, "Second to align to start baking.")
 	intervalSecFlag := flag.Uint("interval_sec", 5, "Interval in seconds to draw each Y axis.")
 
 	flag.Parse()
@@ -22,6 +22,15 @@ func main() {
 	skipPrepare := *skipPrepareFlag
 	alignSec := *alignSecFlag
 	intervalSec := *intervalSecFlag
+
+	if len(imagePath) == 0 {
+		_ = fmt.Errorf("Image path must not be empty.\n")
+		return
+	}
+	if len(db) == 0 {
+		_ = fmt.Errorf("Database connection string must not be empty.\n")
+		return
+	}
 
 	fmt.Println("Making baker.")
 	baker, err := MakeBaker(name, imagePath, db)
@@ -40,13 +49,11 @@ func main() {
 		fmt.Println("Preparation done.")
 	}
 
-	if alignSec >= 0 {
-		fmt.Println(fmt.Sprintf("Aligning to %d second.", alignSec))
-		baker.AlignTime(alignSec)
-		fmt.Println("Aligned.")
-	}
+	fmt.Sprintln("Aligning to", alignSec, "second.")
+	baker.AlignTime(alignSec % 60)
+	fmt.Println("Aligned.")
 
-	fmt.Println(fmt.Sprintf("Start baking at interval %d seconds.", intervalSec))
+	fmt.Sprintln("Start baking at interval", intervalSec, "seconds.")
 	err = baker.Bake(intervalSec)
 	if err != nil {
 		panic(err)
